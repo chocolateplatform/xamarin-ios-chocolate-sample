ba842c3e3ad184d0f37895f6ae045482e21ed29e
Automatic CI Releaser <ltrubov@chocolateplatform.com>
2019-08-12 17:59:08 -0700

    sample xamarin project, working interstitial and reward ads

---
+++ ChocolateSample/ChocolateSample/ViewController.cs
@@ -0,0 +1,121 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using ChocolateSDK;
+
+namespace ChocolateSample
+{
+    public partial class ViewController : UIViewController, IChocolatePlatformInterstitialAdDelegate, IChocolatePlatformRewardAdDelegate
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        private ChocolatePlatformInterstitialAdDisplay iad;
+        private ChocolatePlatformRewardAdDisplay rad;
+
+        public UIViewController RewardAdViewControllerForPresentingModalView => this;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            LoadIntBtn.TouchUpInside += (object sender, EventArgs e) => { LoadIntAd(); };
+            ShowIntBtn.TouchUpInside += (object sender, EventArgs e) => { ShowIntAd(); };
+            ShowIntBtn.Enabled = false;
+            LoadRwdBtn.TouchUpInside += (object sender, EventArgs e) => { LoadRwdAd(); };
+            ShowRwdBtn.TouchUpInside += (object sender, EventArgs e) => { ShowRwdAd(); };
+            ShowRwdBtn.Enabled = false;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        //Mark - UI
+        public void LoadIntAd()
+        {
+            iad = new ChocolatePlatformInterstitialAdDisplay(ChocolatePlatform.AdUnitID, this);
+            iad.Load();
+        }
+
+        public void ShowIntAd()
+        {
+            iad.ShowFrom(this);
+        }
+
+        public void LoadRwdAd()
+        {
+            rad = new ChocolatePlatformRewardAdDisplay(ChocolatePlatform.AdUnitID, this);
+            rad.Load();
+        }
+
+        public void ShowRwdAd()
+        {
+            ChocolatePlatformRewardAdSettings settings = ChocolatePlatformRewardAdSettings.BlankSettings();
+            settings.RewardAmount = 100;
+            settings.RewardName = "hippos";
+            rad.Show(settings);
+        }
+
+        // interstitial delegate interface
+
+        public void OnInterstitialLoaded(ChocolatePlatformInterstitialAdDisplay interstitialAd)
+        {
+            ShowIntBtn.Enabled = true;
+        }
+
+        public void OnInterstitialFailed(ChocolatePlatformInterstitialAdDisplay interstitialAd, ChocolatePlatformNoAdReason errorCode)
+        {
+            Console.Write("failed to load interstitial");
+        }
+
+        public void OnInterstitialShown(ChocolatePlatformInterstitialAdDisplay interstitialAd)
+        {
+            Console.Write("interstitial shown");
+        }
+
+        public void OnInterstitialClicked(ChocolatePlatformInterstitialAdDisplay interstitialAd)
+        {
+            Console.Write("interstitial clicked");
+        }
+
+        public void OnInterstitialDismissed(ChocolatePlatformInterstitialAdDisplay interstitialAd)
+        {
+            ShowIntBtn.Enabled = false;
+        }
+
+        //reward ad delegate
+        public void RewardedVideoDidLoadAd(ChocolatePlatformRewardAdDisplay rewardedAd)
+        {
+            ShowRwdBtn.Enabled = true;
+        }
+
+        public void RewardedVideoDidFailToLoadAdWithError(int error, ChocolatePlatformRewardAdDisplay rewardedAd)
+        {
+            Console.Write("failed to load reward");
+        }
+
+        public void RewardedVideoDidStartVideo(ChocolatePlatformRewardAdDisplay rewardedAd)
+        {
+            Console.Write("starting rewarded video");
+        }
+
+        public void RewardedVideoDidFailToStartVideoWithError(int error, ChocolatePlatformRewardAdDisplay rewardedAd)
+        {
+            Console.Write("failed to start reward");
+        }
+
+        public void RewardedVideoWillDismiss(ChocolatePlatformRewardAdDisplay rewardedAd)
+        {
+
+        }
+
+        public void RewardedVideoDidFinish(nuint rewardAmount, string rewardName)
+        {
+            ShowRwdBtn.Enabled = false;
+        }
+    }
+}
---
+++ ChocolateSample/ChocolateSample/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ChocolateSample
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoadIntBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoadRwdBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ShowIntBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ShowRwdBtn { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoadIntBtn != null) {
+                LoadIntBtn.Dispose ();
+                LoadIntBtn = null;
+            }
+
+            if (LoadRwdBtn != null) {
+                LoadRwdBtn.Dispose ();
+                LoadRwdBtn = null;
+            }
+
+            if (ShowIntBtn != null) {
+                ShowIntBtn.Dispose ();
+                ShowIntBtn = null;
+            }
+
+            if (ShowRwdBtn != null) {
+                ShowRwdBtn.Dispose ();
+                ShowRwdBtn = null;
+            }
+        }
+    }
+}
